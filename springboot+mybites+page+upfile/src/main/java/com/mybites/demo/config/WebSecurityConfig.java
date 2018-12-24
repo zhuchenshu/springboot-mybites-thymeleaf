d2a8b9a7bdb1d35149b3fd89145e250d11671c5e
zhuchenshu <l>
2018-12-24 22:05:05 +0800

    spring security 数据库配置

--- springboot+mybites+page+upfile/src/main/java/com/mybites/demo/config/WebSecurityConfig.java
+++ springboot+mybites+page+upfile/src/main/java/com/mybites/demo/config/WebSecurityConfig.java
@@ -1,14 +1,20 @@
 package com.mybites.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Md4PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 /**
  * * Created with IntelliJ IDEA.
@@ -20,6 +26,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -35,16 +44,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("123123")
-                        .roles("USER")
-                        .build();
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("123123")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
-        return new InMemoryUserDetailsManager(user);
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 密码使用md4加密
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select user_name, password ,true from user_t where user_name = ?")
+                .authoritiesByUsernameQuery("select user_name,'RULE_USER' from user_t where user_name = ?")
+                .passwordEncoder(new Md4PasswordEncoder());
     }
 }
