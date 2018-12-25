a1aca7fd0fa0a70dba232524483dd211d656729a
zhuchenshu <l>
2018-12-25 21:30:38 +0800

    添加readme

--- springboot+mybites+page+upfile/src/main/java/com/mybites/demo/config/WebSecurityConfig.java
+++ springboot+mybites+page+upfile/src/main/java/com/mybites/demo/config/WebSecurityConfig.java
@@ -31,17 +31,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
+        http.formLogin()
                 .loginPage("/login")
-                .permitAll()
                 .and()
                 .logout()
-                .permitAll();
+                .and().rememberMe().tokenValiditySeconds(300).key("demoKey");
     }
 
 //    @Bean
